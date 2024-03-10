02b788da204c230f8111ff84739108b778e46ace
MuhammadAhmad-1 <161146898+MuhammadAhmad-1@users.noreply.github.com>
2024-03-10 14:11:13 +0000

    changed script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-const inputbox = document.getElementById("input");
-const tasks = document.getElementById("ul");
-
-function addTask(){
-    if(inputbox.value === ''){
-        alert("you must write something!");
-    }
-    else{
-        let li =document.createElement("li");
-        li.innerHtml = inputbox.value;
-        tasks.appendChild(li);
-    }
-}
+let button = document.getElementById('add');
+button.addEventListener("click", ()=>{
+    alert('Hello,');
+});
